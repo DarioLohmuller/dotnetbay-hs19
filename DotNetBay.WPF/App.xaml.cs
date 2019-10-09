c0fccee91f221f38e4adc7a4f6798541f7f83bbf
Dario Lohmuller <dario.lohmuller@gmail.com>
2019-10-09 17:50:27 +0200

    started with wpf

---
+++ DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DotNetBay.Core.Execution;
+using DotNetBay.Data.Provider.FileStorage;
+using DotNetBay.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Data.Entity;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly IMainRepository MainRepository = new FileSystemMainRepository("WPF_Repository");
+        public readonly IAuctionRunner AuctionRunner = new AuctionRunner(MainRepository);
+
+        public App()
+        {
+            this.AuctionRunner.Start();
+            LoadDemoData();
+        }
+
+        private static void LoadDemoData()
+        {
+            var memberService = new SimpleMemberService(MainRepository);
+            var service = new AuctionService(MainRepository, memberService);
+
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+    }
+}
