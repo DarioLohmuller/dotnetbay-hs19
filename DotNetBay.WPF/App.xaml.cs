5187084a8b5e2ffa9a8fe96a98415a667ed51873
Pascal Oertli <pascal.oertli@students.fhnw.ch>
2019-10-27 12:43:12 +0100

    Layout added

---
+++ DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.Entity;
+using DotNetBay.Data.Provider.FileStorage;
+using DotNetBay.Interfaces;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly IMainRepository MainRepository = new FileSystemMainRepository("MainRepository");
+        public readonly IAuctionRunner AuctionRunner;
+
+        public App()
+        {
+            AuctionRunner = new AuctionRunner(MainRepository);
+            this.AuctionRunner.Start();
+
+            var memberService = new SimpleMemberService(MainRepository);
+            var service = new AuctionService(MainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+    }
+}
